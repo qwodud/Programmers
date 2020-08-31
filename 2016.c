67fb20beb8ca55c6487771637afd3a74cc4f3438
qwodud <qwodud@kakao.com>
2020-08-31 23:00:34 +0900

    2016 commit

---
+++ 2016.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int a, int b) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    char* answer = (char*)malloc(3);
+
+    const char* week[]={"THU","FRI","SAT","SUN","MON","TUE","WED"};
+    int month[12]={31,29,31,30,31,30,31,31,30,31,30,31};
+    int day=0;
+
+    for(int i=0; i<a-1; i++) day+=month[i];  
+
+    day+=b;
+    answer = week[day%7];
+
+    return answer;
+    free(answer);
+}
